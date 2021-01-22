893b0723185897600973cf727d0dde42d48c2340
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-01-22 17:26:22 +0000

    5 day forcast added

--- Assets/script.js
+++ Assets/script.js
@@ -1,32 +1,25 @@
 $(document).ready(function () {
 
+
+
     var cityArr = [];
     var searchInput = $("#searchInput");
-    var city = "London";
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=fb4315cea4eb938c59ecfe1bbed51784&units=metric";
+    var city = "london";
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&exclude=hourly&appid=fb4315cea4eb938c59ecfe1bbed51784&units=metric";
 
-
-    console.log(queryURL);
-
-    var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=fb4315cea4eb938c59ecfe1bbed51784&units=metric";
+    var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=fb4315cea4eb938c59ecfe1bbed51784&units=metric";
     var lat = "";
     var lon = "";
 
-
-    // function createHTML (className, response) {
-    //     $("." + className + " ").HTML
-    // }
+    console.log(queryURL);
 
 
-    // function createText(text, response, string) {
-    //     $("." + text + " ").text(" " + string + " " + response);
-    // }
 
     function callAPIs() {
 
 
 
-        
+
         // city = searchInput.val().trim();
         console.log(city);
 
@@ -36,34 +29,58 @@ $(document).ready(function () {
         }).then(function (response) {
             console.log(response);
             console.log(response.city.name);
+            // Current day 
             $("#cityWeather").text("Today's Weather in " + response.city.name)
+            var today = moment().format("L");
+            $("#todayDate").text(today);
 
             console.log(response.list[0].weather[0].icon);
             $("#weatherIcon").attr("src", "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
-            
+
             console.log(response.list[0].main.temp);
             $("#cityTemp").text("Temperature: " + response.list[0].main.temp + "°C");
-           
+
             console.log(response.list[0].main.humidity);
             $("#cityHumid").text("Humidity: " + response.list[0].main.humidity + "%");
-            
+
             console.log(response.list[0].wind.speed);
             $("#cityWind").text("Wind Speed: " + response.list[0].wind.speed + "mph");
-            
+
             console.log(response.city.coord.lat);
             console.log(response.city.coord.lon);
             lat = response.city.coord.lat;
             lon = response.city.coord.lon;
 
-        });
-
-        $.ajax({
-            url: queryURL2,
-            method: "GET",
-        }).then(function (response2) {
-            console.log(response2);
+            // Next 5 days
+            // dates
             
-        });
+            $("#dateDay1").text(moment().add(1, "days").format("L"));
+            $("#dateDay2").text(moment().add(2, "days").format("L"));
+            $("#dateDay3").text(moment().add(3, "days").format("L"));
+            $("#dateDay4").text(moment().add(4, "days").format("L"));
+            $("#dateDay5").text(moment().add(5, "days").format("L"));
+
+
+            var dayNum = 0;
+
+            //iterate through the 40 weather data sets
+            for (var i = 0; i < response.list.length; i++) {
+                $("#tempDay" + dayNum).text("Temperature: " + response.list[i].main.temp + "°C");
+                $("#humidDay"+ dayNum).text("Humidity: " + response.list[i].main.humidity + "%");
+                $("#iconDay" + dayNum).attr("src", "http://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png");
+                dayNum++;
+
+            }
+
+
+                $.ajax({
+                    url: queryURL2,
+                    method: "GET",
+                }).then(function (response2) {
+                    console.log(response2);
+
+                });
+            });
     };
 
     $("button").on("click", function (event) {
